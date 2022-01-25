0576120f1f404b2091cc7c85edf624085e1e60b4
Olartbaraq <akanbi.ges201@gmail.com>
2022-01-26 00:08:25 +0100

    updated the class with a dictionary representation

--- models/base_model.py
+++ models/base_model.py
@@ -14,9 +14,16 @@ class BaseModel():
     """Declared a class with attributes and methods"""
     def __init__(self, *args, **kwargs):
         """instantiation of class BaseModel"""
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.now()
-    self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+        # setattr() function sets the value of the attribute of an object
 
     def __str__(self):
         """prints the class attributes as a string"""
